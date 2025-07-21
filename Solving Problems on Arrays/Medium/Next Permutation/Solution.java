21618cb86b6c0aa045eb9c24692a67b7542142c4
Gerry Summers <Singur95878@student.edmonds.edu>
2025-07-20 22:47:43 -0700

    added a new solution

---
+++ Solving Problems on Arrays/Medium/Next Permutation/Solution.java
@@ -0,0 +1,42 @@
+
+public class Solution {
+    public void nextPermutation(int[] arr){
+        //find the pivot
+        int pivot = -1;
+        int n = arr.length;
+
+        for(int i=n-2; i>=0; i--){
+            if(arr[i] < arr[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+
+        if(pivot == -1){
+            reverse(arr, 0, n-1);
+        }
+
+        for(int i=n-1; i>pivot; i--){
+            if(arr[i] > arr[pivot]){
+                swap(arr, i, pivot);
+                break;
+            }
+        }
+
+        reverse(arr, pivot+1, n-1);
+    }
+
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void reverse(int[] arr, int start, int end){
+        while(start < end){
+            swap(arr, start, end);
+            start ++;
+            end--;
+        }
+    }
+}
