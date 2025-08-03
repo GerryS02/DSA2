302d843552a620762a6e2e8846447418c309c624
Gerry Summers <Singur95878@student.edmonds.edu>
2025-08-03 14:36:38 -0700

    added a new solution

---
+++ Binary Search/BS on 1D Arrays/Upper Bound/Solution.java
@@ -0,0 +1,32 @@
+// Given a sorted array of nums and an integer x, write a program to find the upper bound 
+// of x.
+
+// The upper bound algorithm finds the first and smallest index in a sorted array where the 
+// value at that index is greater than a given key i.e. x.
+
+// If no such index is found, return the size of the array.
+
+class Solution {
+    public int upperBound(int[] nums, int x){
+        int lo = 0;
+        int hi = nums.length-1;
+
+        int ans = nums.length; //default answer if no element > x is found
+
+        while(lo <= hi){
+            int mid = (lo+hi)/2;
+
+            //if nums[mid] is greater than x,
+            //it could be a possible answer, but we try to find a smaller index
+            if(nums[mid] > x){
+                ans = mid;
+                hi = mid-1;
+            } else{
+                //if nums[mid] is less than or equal to x, search the right half
+                lo = mid+1;
+            }
+        }
+
+        return ans;
+    }
+}
