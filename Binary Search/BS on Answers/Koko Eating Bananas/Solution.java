008a26620868ba3a83e3f3980bfe0caed8bfcfc6
Gerry Summers <Singur95878@student.edmonds.edu>
2025-08-10 10:28:00 -0700

    modified solution

--- Binary Search/BS on Answers/Koko Eating Bananas/Solution.java
+++ Binary Search/BS on Answers/Koko Eating Bananas/Solution.java
@@ -14,36 +14,43 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int max = 0;
 
+        // Find the maximum number of bananas in a pile
+        // This will be the upper limit of Koko's eating speed
         for(int val : piles){
             max = Math.max(val, max);
         }
 
-        int lo = 0;
-        int hi = max;
+        int lo = 1;  // Minimum possible speed (can't be 0 or division by zero error)
+        int hi = max; // Maximum possible speed
+        int ans = Integer.MAX_VALUE; // Store the smallest possible eating speed
 
-        int ans = Integer.MAX_VALUE;
-
+        // Binary search for the minimum eating speed
         while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
+            int mid = lo + (hi - lo) / 2; // Middle speed to check
 
+            // If Koko can eat all bananas at this speed within h hours
             if(isPossible(piles, mid, h)){
-                ans = mid;
-                hi = mid-1;
-            } else{
-                lo = mid+1;
+                ans = mid;    // Save the answer
+                hi = mid - 1; // Try to find a smaller speed
+            } else {
+                lo = mid + 1; // Increase speed to finish in time
             }
         }
 
-        return ans;
+        return ans; // Return the minimum speed found
     }
 
     public boolean isPossible(int[] piles, int mid, int h){
-        long time = 0;
+        long time = 0; // Total hours needed
 
-        for(int i=0;  i<piles.length; i++){
-            time += (int) Math.ceil((double) piles[i]/mid);
+        // Calculate total hours needed to eat all piles at 'mid' speed
+        for(int i = 0; i < piles.length; i++){
+            // Math.ceil ensures Koko needs a full extra hour if bananas remain
+            time += (int) Math.ceil((double) piles[i] / mid);
         }
 
+        // If time is within allowed hours, return true
         return time <= h;
     }
 }
+
