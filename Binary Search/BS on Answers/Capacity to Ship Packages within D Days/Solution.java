0c1f620b49603df1f370065f9c9c9a41f6dab4e1
Gerry Summers <Singur95878@student.edmonds.edu>
2025-08-10 10:26:22 -0700

    modified the solution

--- Binary Search/BS on Answers/Capacity to Ship Packages within D Days/Solution.java
+++ Binary Search/BS on Answers/Capacity to Ship Packages within D Days/Solution.java
@@ -9,49 +9,57 @@
 
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int sum = 0;
-        int max = 0;
+        int sum = 0; // total weight of all packages
+        int max = 0; // weight of the heaviest package
 
-        for(int val : weights){
+        // Find total sum and max weight
+        for (int val : weights) {
             sum += val;
             max = Math.max(val, max);
         }
 
-        if(weights.length == days){
-            return max;
+        // If there are as many days as packages,
+        // we can ship one package per day
+        if (weights.length == days) {
+            return max; // capacity must be the heaviest package
         }
 
-        int lo = max;
-        int hi = sum;
-        int ans = 0;
+        // Search space for binary search
+        int lo = max; // minimum possible capacity (at least the heaviest package)
+        int hi = sum; // maximum possible capacity (ship all in one day)
+        int ans = 0;  // store the smallest capacity found
 
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
+        // Binary search to find minimum ship capacity
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2; // try this capacity
 
-            if(isPossible(weights, mid, days)){
-                ans = mid;
-                hi = mid-1;
-            } else{
-                lo = mid+1;
+            // Check if we can ship within given days using this capacity
+            if (isPossible(weights, mid, days)) {
+                ans = mid;     // store as potential answer
+                hi = mid - 1;  // try smaller capacity
+            } else {
+                lo = mid + 1;  // need more capacity
             }
         }
 
         return ans;
     }
 
-    public boolean isPossible(int[] weights, int mid, int days){
-        int day = 1;
-        int sum = 0;
+    public boolean isPossible(int[] weights, int mid, int days) {
+        int day = 1; // start from day 1
+        int sum = 0; // weight for current day
 
-        for(int i=0; i<weights.length; i++){
+        for (int i = 0; i < weights.length; i++) {
             sum += weights[i];
 
-            if(sum > mid){
-                day++;
-                sum = weights[i];
+            // If adding this package exceeds capacity
+            if (sum > mid) {
+                day++;            // move to next day
+                sum = weights[i]; // start with this package on new day
             }
         }
 
+        // Return true if we can ship within allowed days
         return day <= days;
     }
 }
