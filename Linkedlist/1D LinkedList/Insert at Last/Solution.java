1701d490c24a90e6e8d876a27d32b76b577237a6
Gerry Summers <Singur95878@student.edmonds.edu>
2025-09-05 21:21:41 -0700

    added a new function

--- Linkedlist/1D LinkedList/Insert at Last/Solution.java
+++ Linkedlist/1D LinkedList/Insert at Last/Solution.java
@@ -1,9 +1,26 @@
 import Linkedlist.Node;
+import static Linkedlist.LinkedList.head;
 
 public class Solution {
-    public Node head;
+    
+    public void insertAtLast(int data){
+        Node newNode = new Node(data); //create a new node
 
-    public Solution(){
-      head = null;  // Initialize head to null (empty list)
+        //if the list is empty, make the new node the head node and return
+        if(head == null){
+            newNode = head;
+            return;
+        }
+
+        //point the head to current node
+        Node currNode = head;
+
+        //while the current node is not the last node
+        while(currNode.next != null){
+            currNode = currNode.next; //increment the current node
+        }
+
+        //point the next of the current node to the new node
+        currNode.next = newNode;
     }
 }
