3206294fb80848f76d2c99fb3dd14a646cb94c04
Gerry Summers <Singur95878@student.edmonds.edu>
2025-09-07 13:58:26 -0700

    fix the code

--- Linkedlist/1D LinkedList/Length of the LinkedList (traversal)/Solution.java
+++ Linkedlist/1D LinkedList/Length of the LinkedList (traversal)/Solution.java
@@ -7,9 +7,11 @@ public class Solution {
         //if the list is empty, the length is 0
         if(head == null) return 0;
 
-        //while the list is empty
-        while(head != null){
-            head = head.next; //move the head forward
+        Node curr = head; //pointer to track the nodes
+
+        //traverse the list until the end
+        while(curr != null){
+            curr = curr.next; //move to the next node
             len++; //increment the length
         }
 
