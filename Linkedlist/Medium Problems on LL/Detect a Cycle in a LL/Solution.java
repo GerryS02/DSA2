12e75bb12c4b33a2077af9ee5e1b13f989f308f9
Gerry Summers <Singur95878@student.edmonds.edu>
2025-09-08 12:17:45 -0700

    Added a new solution

---
+++ Linkedlist/Medium Problems on LL/Detect a Cycle in a LL/Solution.java
@@ -0,0 +1,19 @@
+import Linkedlist.Node;
+
+public class Solution {
+    public boolean hascycle(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+
+        return false;
+    } 
+}
