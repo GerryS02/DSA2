8a400072f470592746aa101bc72f79db23c127c4
Gerry Summers <Singur95878@student.edmonds.edu>
2025-09-24 11:12:50 -0700

    added  anew solution

--- BinaryTree/Medium Problems/Height of Binary Tree/Solution.java
+++ BinaryTree/Medium Problems/Height of Binary Tree/Solution.java
@@ -2,6 +2,11 @@ import BinaryTree.Node;
 
 public class Solution {
     public int height(Node root){
-        
+        if(root == null) return 0;
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return (Math.max(lh, rh) +1);
     }
 }
