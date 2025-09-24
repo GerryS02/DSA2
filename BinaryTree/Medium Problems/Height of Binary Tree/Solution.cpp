3602a73637d94e3a41a83970e99de031afdf31bb
Gerry Summers <Singur95878@student.edmonds.edu>
2025-09-24 11:07:59 -0700

    Added  anew solution

---
+++ BinaryTree/Medium Problems/Height of Binary Tree/Solution.cpp
@@ -0,0 +1,13 @@
+#include "Node.h"
+
+class Soltuion{
+public:
+    int height(Node* root){
+        if(root == NULL) return 0;
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        return (max(lh, rh) +1);
+    }
+};
