ad63be5b0c23ccfc7e3a0b77a667156ee38b3aae
Gerry Summers <Singur95878@student.edmonds.edu>
2025-09-29 12:48:22 -0700

    added a new solution

---
+++ BinaryTree/Medium Problems/Check if Two Trees are Identical/Solution.cpp
@@ -0,0 +1,15 @@
+#include "Node.h"
+
+class Solution{
+public:
+    bool isSameTree(Node* p, Node* q){
+        if(p == NULL || q == NULL){
+            return p == q;
+        }
+
+        bool isLeft = isSameTree(p->left, q->left);
+        bool isRight = isSameTree(p->right, q->right);
+
+        return isLeft && isRight && (p->val == q->val);
+    }
+};
