fdcd2823709aa513a4d13dd1f362008bdae93664
Gerry Summers <Singur95878@student.edmonds.edu>
2026-02-24 11:54:58 -0800

    insert intervals greedy solution

---
+++ Greedy Algorithms/Medium/Insert Interval/Solution.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval){
+        ArrayList<int[]> ans = new ArrayList<>();
+
+        int n = intervals.length;
+        int i = 0;
+
+        while(i<n && intervals[i][1] < newInterval[0]){
+            ans.add(intervals[i]);
+
+            i++;
+        }
+
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.min(intervals[i][1], newInterval[1]);
+
+            i++;
+        }
+
+        ans.add(newInterval);
+
+        while(i<n){
+            ans.add(intervals[i]);
+
+            i++;
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
